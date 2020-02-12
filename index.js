ed789ced7f16cd9e53808ca00fa74ac05e82e2a2
pragmatic-distribution-6705 <philipmkatz@gmail.com>
2020-02-12 21:42:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+const arr = [];
+for(var i = 0; musicians.length > i ; i++){ 
+arr.push(`${musicians[i]} plays ${instruments[i]}`); 
+}
+return arr;
+}
+
+
+const facts = [
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"
+
+]
+
+function johnLennonFacts() {
+let arr = []; 
+let i = 0;
+while(i < facts.length); {
+arr.push(`${facts[i]}!!!`)
+i++
+}
+return arr;
+}
+
+johnLennonFacts();
