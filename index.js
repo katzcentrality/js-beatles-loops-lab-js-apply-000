5f9438b5478beb4c263dfb786359229dd71a798f
pragmatic-distribution-6705 <philipmkatz@gmail.com>
2020-02-12 21:47:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function theBeatlesPlay(musicians, instruments) {
+const arr = [];
+for(var i = 0; musicians.length > i ; i++){ 
+arr.push(`${musicians[i]} plays ${instruments[i]}`); 
+}
+return arr;
+}
+
+
+const facts = [
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"
+
+]
+
+function johnLennonFacts() {
+let arr = []; 
+let i = 0;
+while(i < facts.length); {
+arr.push(`${facts[i]}!!!`)
+i++
+}
+return arr;
+}
+
+function iLoveTheBeatles(i) {
+let arr = [];
+do {
+arr.push('I love the Beatles!');
+i++
+}
+while (i < 15);
+return arr;
+}
